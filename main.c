d48d9592f500b10771db69afb1845a0b9742331f
Connor Hanson <chanson@royal-11.cs.wisc.edu>
2020-09-20 12:45:53 -0500

    proper error message

--- main.c
+++ main.c
@@ -9,22 +9,29 @@
 #include "optproc.h"
 #include "parser.h"
 
+void print_usage() {
+    printf("Usage: \n");
+    printf("\tps [options]\n");
+    printf("Options: \n\t-p <pid>: print process info for <pid>\n");
+    printf("\t-s: print single character state info about process\n");
+    printf("\t-U: print user time consumed by process. Enabled by default. -U- to disable\n");
+    printf("\t-S: print system time consumed by process\n");
+    printf("\t-v: print virtual memory consumed by process (in pages)\n");
+    printf("\t-c: print command line which started process. Enabled by default. -c- to disable\n");
+}
+
 int main(int argc, char *argv[]) {
 
     // display option flags
-    /*pflag = 0; // pid
-    sflag = 0; // single state character info
-    Uflag = 1; // user time consumed by process
-    Sflag = 0; // sys time consumed by proc
-    vflag = 0; // VRAM in pages used by proc
-    cflag = 1; // CLA which started process*/
-
+    // p, s, U, S, v, c, -p counter
     int flags[] = {0, 0, 1, 0, 0, 1};
 
     int resp = set_flags(argc, argv, flags);
     
     if (resp == -1) { //error
+        print_usage();
         printf("Error setting flags\n");
+        return -1;
     }
 
     // check flags
--- optproc.c
+++ optproc.c
@@ -1,4 +1,5 @@
 //Author: Connor Hanson, Tiger Ji
+// Processes the options given in the command line by the user
 
 #include <dirent.h>
 #include <unistd.h>
@@ -17,16 +18,13 @@ int set_flags(int argc, char* argv[], int flags[]) {
 
     while ((c = getopt(argc, argv, "p:s::U::S::v::c::")) != -1) {
         switch (c) {
-            // unexpected character
-            case '?': 
-                return -1;
-
             // TODO allow multiple -p
-            case 'p':
+            case 'p':       
                 flags[0] = 1;
                 pid = atoi(optarg);
                 flags[0] = pid; // FIXME, check for no input
-                if (pid == 0) { // cannot be converted to int
+                if (pid == 0) { // cannot be converted to int, includes spaces
+                    printf("Error, pid could not be converted to int");
                     return -1;
                 }
                 printf("%d \n", pid);
@@ -34,7 +32,6 @@ int set_flags(int argc, char* argv[], int flags[]) {
 
             case 's': 
                 flags[1] = 1; 
-
                 // check strlen to only allow -s & -s- to be valid
                 if (optarg != NULL && (strlen(optarg) == 1)) {
                     flags[1] = 0;
@@ -56,10 +53,18 @@ int set_flags(int argc, char* argv[], int flags[]) {
 
             case 'S': 
                 flags[3] = 1;
+                if (optarg != NULL && strlen(optarg) > 1) {
+                    fprintf(stderr, "Syntax error: -S option");
+                    return -1;
+                }
             break;
 
             case 'v':
                 flags[4] = 1;
+                if (optarg != NULL && strlen(optarg) > 1) {
+                    fprintf(stderr, "Syntax error: -v option");
+                    return -1;
+                }
             break;
 
             case 'c':
@@ -71,6 +76,10 @@ int set_flags(int argc, char* argv[], int flags[]) {
                     return -1;
                 }
             break;
+
+            // unexpected character
+            case '?': 
+                return -1;
 
                 // don't wanna crash if something unexpected
             default: 
