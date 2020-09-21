377fcbd69bd85a3bf1b5b556c0c44fea17e3ca55
reatreify <70864815+reatreify@users.noreply.github.com>
2020-09-21 17:23:03 -0500

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,6 +1,12 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+void print_header(int flags[]);
+
+int print_info(int pid, int flags[]);
+
+int iterate_proc();
+
 char getState(int id);
 
 int getUtime(int id);
@@ -10,7 +16,5 @@ int getStime(int id);
 int getVm(int id);
 
 void printCmd(int id);
-
-int iterate_proc(int flags[]);
 
 #endif
